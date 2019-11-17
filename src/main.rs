47bf09236d3aaddf4e29e01d800e84a56514d1f4
Deshan Basura <deshanbasura3@gmail.com>
2019-11-17 07:05:08 -0500

    Just add infinite loop to give more chances on guessing the number

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main(){
 
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1,101);
+    let secret_number = rand::thread_rng().gen_range(1,11);
 
     println!("Your secret number is {}", secret_number);
     println!("Please input your guess..");
@@ -20,12 +20,15 @@ fn main(){
     let guess: u32 = guess.trim().parse()
     .expect("Please type a number!");
 
-    println!("You guessed {}", guess);
+    loop {
+        
+        println!("You guessed {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }unimplemented!();
     }
 
 }
