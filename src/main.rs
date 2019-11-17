562b81e46618412b9f0c3f1fa9410570b89931b0
Deshan Basura <deshanbasura3@gmail.com>
2019-11-17 07:17:11 -0500

    Corrected loop issue and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,34 @@
 use std::io;
-use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
+use rand::Rng;
 
-
-
-fn main(){
-
+fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1,11);
-
-    println!("Your secret number is {}", secret_number);
-    println!("Please input your guess..");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("Failed to readline");
-
-    let guess: u32 = guess.trim().parse()
-    .expect("Please type a number!");
+    let secret_number = rand::thread_rng().gen_range(1, 11);
 
     loop {
-        
-        println!("You guessed {}", guess);
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
-
-
 }
